1421447a2c27787298c799fd04ef093dfbab8c5c
thanhhuyenle <huyenmotnang@gmail.com>
2022-01-16 15:50:16 +0700

    fix menu in sidebar

--- src/pages/Sidebar.vue
+++ src/pages/Sidebar.vue
@@ -17,8 +17,8 @@
         <v-row class="flex-column mt-4">
           <v-list-item>
             <v-list-item-content class="text-center">
-              <v-list-item-title class="text-uppercase">{{tag.name}}</v-list-item-title>
-              <v-list-item-subtitle>{{tag.id}}</v-list-item-subtitle>
+              <v-list-item-title class="text-uppercase">{{ tag.name }}</v-list-item-title>
+              <v-list-item-subtitle>{{ tag.id }}</v-list-item-subtitle>
             </v-list-item-content>
           </v-list-item>
         </v-row>
@@ -29,9 +29,9 @@
             class="mt-10"
         >
           <v-list-item
-              v-for="(item, i) in items"
+              v-for="(item, i) in menuItems"
               :key="i"
-              @click.once="selectPage(item.link)"
+              :to="item.to"
           >
             <v-list-item-icon>
               <v-icon v-text="item.icon"></v-icon>
@@ -55,34 +55,61 @@ import {mapActions} from "vuex";
 
 export default {
   name: "Sidebar",
-  data (){
+  data() {
     return {
-      selectedItem: 0
+      selectedItem: 0,
+      menuItems: [],
+      studentItems: [
+        {text: 'Trang chủ', icon: 'mdi-home', to: "/homepage"},
+        {text: 'Sơ yếu lý lịch', icon: 'mdi-account', to: "/profile"},
+        {text: 'Chấm điểm rèn luyện', icon: 'mdi-pen', to: "/"},
+        {text: 'Xem điểm rèn luyện', icon: 'mdi-heart', to: "/resultsHistory"},
+      ],
+      monitorItems: [
+        {text: 'Trang chủ', icon: 'mdi-home', to: "/homepage"},
+        {text: 'Sơ yếu lý lịch', icon: 'mdi-account', to: "/profile"},
+        {text: 'Chấm điểm rèn luyện', icon: 'mdi-pen', to: "/"},
+        {text: 'Xem điểm rèn luyện', icon: 'mdi-heart', to: "/resultsHistory"},
+        {text: 'Danh sách sinh viên', icon: 'mdi-account-group', to: "/listMembersMonitor"},
+        {text: 'Danh sách lớp', icon: 'mdi-format-list-numbered', to: "/listClassesMoitor"},
+      ],
+      teacherItems: [
+        {text: 'Trang chủ', icon: 'mdi-home', to: "/homepage"},
+        {text: 'Sơ yếu lý lịch', icon: 'mdi-account', to: "/profile"},
+        {text: 'Danh sách sinh viên', icon: 'mdi-account-group', to: "/listMembersTeacher"},
+        {text: 'Danh sách lớp', icon: 'mdi-format-list-numbered', to: "/listClassesTeacher"},
+      ],
+      departmentItems: [
+        {text: 'Trang chủ', icon: 'mdi-home', to: "/homepage"},
+        {text: 'Sơ yếu lý lịch', icon: 'mdi-account', to: "/profile"},
+        {text: 'Danh sách lớp', icon: 'mdi-format-list-numbered', to: "/listClassesDepartment"},
+        {text: 'Danh sách giảng viên', icon: 'mdi-account-multiple', to: "/"},
+      ],
+      adminItems: [
+        {text: 'Trang chủ', icon: 'mdi-home', to: "/homepage"},
+        {text: 'Phiếu xét điểm rèn luyện', icon: 'mdi-file', to: "/"},
+        {text: 'Danh sách khoa', icon: 'mdi-format-list-bulleted', to: "/listFaculties"},
+        {text: 'Quản lý thời gian', icon: 'mdi-clock', to: "/manageTime"},
+      ]
     }
   },
   created() {
-    this.getMenu()
     this.getTag()
+    this.getData()
   },
   computed: {
-    items(){
-     return  this.$store.state.menu
-    },
-    tag(){
+    tag() {
       return this.$store.state.tag
     }
   },
   methods: {
-    ...mapActions(['getMenu', 'getTag']),
-    selectPage(link){
-      var listPath = ["/profile", "/homepage", "resultsHistory"]
-
-      if(listPath.includes(this.$route.fullPath) && link != this.$route.fullPath){
-          this.$router.push(link)
-      }else {
-        this.$router.push(link+ this.tag.role)
-      }
-      return
+    ...mapActions(['getTag']),
+    getData(){
+      if(this.tag.role == "Student") this.menuItems= this.studentItems
+      if(this.tag.role == "Monitor") this.menuItems= this.monitorItems
+      if(this.tag.role == "Teacher") this.menuItems= this.teacherItems
+      if(this.tag.role == "Department") this.menuItems= this.departmentItems
+      if(this.tag.role == "Admin") this.menuItems= this.adminItems
     }
   }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -7,50 +7,13 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
     state: {
-        tag: [{
-            name: "",
-            id: ""
-        }],
-        menu: [
-            {
-                text: 'Trang chủ',
-                icon: 'mdi-home',
-                role: ["Student", "Monitor", "Teacher", "Department", "Admin"],
-                link: "/homepage"
-            },
-            {
-                text: 'Sơ yếu lý lịch',
-                icon: 'mdi-account',
-                role: ["Student", "Monitor", "Teacher", "Department"],
-                link: "/profile"
-            },
-            {text: 'Chấm điểm rèn luyện', icon: 'mdi-pen', role: ["Student", "Monitor"], link: "/homepage"},
-            {text: 'Xem điểm rèn luyện', icon: 'mdi-heart', role: ["Student", "Monitor",], link: "/resultsHistory"},
-            {text: 'Danh sách sinh viên', icon: 'mdi-account-group', role: ["Monitor", "Teacher",], link: "/listMembers"},
-            {
-                text: 'Danh sách lớp',
-                icon: 'mdi-format-list-numbered',
-                role: ["Monitor", "Teacher", "Department"],
-                link: "/listClasses"
-            },
-            {text: 'Danh sách giảng viên', icon: 'mdi-account-multiple', role: ["Department"], link: "/homepage"},
-            {text: 'Phiếu xét điểm rèn luyện', icon: 'mdi-file', role: ["Admin"], link: "/homepage"},
-            {text: 'Danh sách khoa', icon: 'mdi-format-list-bulleted', role: ["Admin"], link: "/listFaculties"},
-            {text: 'Quản lý thời gian', icon: 'mdi-clock', role: ["Admin"], link: "/manageTime"},
-        ]
+        tag: [],
     },
     mutations: {
         GET_TAG(state, decoded) {
             state.tag = decoded
-            return state.menu
+            return state.tag
         },
-        GET_MENU(state, role) {
-            state.menu = state.menu.filter(item => {
-                return item.role.includes(role)
-            })
-
-            return state.menu
-        }
     },
     actions: {
         getTag({commit}) {
@@ -59,12 +22,6 @@ export default new Vuex.Store({
 
             commit('GET_TAG', decoded)
         },
-        getMenu({commit}) {
-            var token = localStorage.getItem("token");
-            var decoded = jwt_decode(token);
-
-            commit('GET_MENU', decoded.role)
-        }
     },
     modules: {}
 })
